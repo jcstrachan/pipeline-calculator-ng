17d50383e33158af702b0646aa9069936a6d14f0
Vartexy <jamescsr10@gmail.com>
2023-03-06 12:53:37 +0000

    Moved over React HTML

--- pipeline-calculator/src/app/app.module.ts
+++ pipeline-calculator/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ParameterInputComponent } from './components/parameter-input/parameter-input.component';
+import { ExportSettingsComponent } from './components/export-settings/export-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParameterInputComponent,
+    ExportSettingsComponent
   ],
   imports: [
     BrowserModule
