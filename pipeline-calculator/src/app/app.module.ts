1bf5402d24fe7ff4ad524c46ff906c4508eeb7df
James Strachan <49254739+jcstrachan@users.noreply.github.com>
2023-03-15 16:09:56 +0000

    Update material

--- pipeline-calculator/src/app/app.module.ts
+++ pipeline-calculator/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ExportSettingsComponent } from './components/export-settings/export-set
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 
 @NgModule({
   declarations: [
