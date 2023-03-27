22c8d35877a52b14d1f6fdd5b1b7c441171d3c12
James Strachan <49254739+jcstrachan@users.noreply.github.com>
2023-03-27 11:47:21 +0100

    Fixed charts to show data

--- pipeline-calculator/src/app/components/data-visualisation/chart/chart.component.ts
+++ pipeline-calculator/src/app/components/data-visualisation/chart/chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { EChartsOption } from 'echarts';
 
 @Component({
@@ -11,54 +11,63 @@ export class ChartComponent {
   @Input() data: number[][] = [];
   @Input() xAxisName: string = 'Length of buoyancysection (m)';
   @Input() yAxisName: string = '';
+
+  chartOption: EChartsOption = {};
   
-  chartOption: EChartsOption = {
-    animation: false,
-    grid: {
-      top: 40,
-      left: 50,
-      right: 40,
-      bottom: 50
-    },
-    xAxis: {
-      name: this.xAxisName,
-      min: 0,
-      max: 100,
-      minorTick: {
-        show: true
+  ngOnChanges(changes: SimpleChanges) {
+      this.updateChart()
+  }
+
+  private updateChart() {
+    this.chartOption = {
+      animation: false,
+      grid: {
+        top: 40,
+        left: 50,
+        right: 40,
+        bottom: 50
       },
-      minorSplitLine: {
-        show: false
+      xAxis: {
+        name: this.xAxisName,
+        min: 0,
+        max: 100,
+        minorTick: {
+          show: true
+        },
+        minorSplitLine: {
+          show: false
+        },
+        nameLocation: 'middle',
+        nameGap: 25,
+        nameTextStyle: {
+          color: '#333333'
+        }
       },
-      nameLocation: 'middle',
-      nameGap: 25,
-      nameTextStyle: {
-        color: '#333333'
-      }
-    },
-    yAxis: {
-      name: this.yAxisName,
-      min: -15,
-      max: 15,
-      minorTick: {
-        show: true
+      yAxis: {
+        name: this.yAxisName,
+        min: -15,
+        max: 15,
+        minorTick: {
+          show: true
+        },
+        minorSplitLine: {
+          show: false
+        },
+        nameLocation: 'middle',
+        nameGap: 25,
+        nameTextStyle: {
+          color: '#333333'
+        }
       },
-      minorSplitLine: {
-        show: false
-      },
-      nameLocation: 'middle',
-      nameGap: 25,
-      nameTextStyle: {
-        color: '#333333'
-      }
-    },
-    series: [
-      {
-        type: 'line',
-        showSymbol: false,
-        clip: true,
-        data: this.data
-      }
-    ]
-  };
+      series: [
+        {
+          type: 'line',
+          showSymbol: false,
+          clip: true,
+          data: this.data
+        }
+      ]
+    };
+  }
+  
 }
